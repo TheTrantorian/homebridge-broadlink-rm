425d401f7b76d5cbb99588102b97bd44bdaa6cb9
TheTrantorian <lionel.vaillant@icloud.com>
2018-02-25 17:45:40 +0100

    Update switch.js
    ping ip every 60sec

--- accessories/switch.js
+++ accessories/switch.js
@@ -21,7 +21,7 @@ class SwitchAccessory extends BroadlinkRMAccessory {
     const { config, debug, log } = this;
     let { pingIPAddress } = config;
 
-    const pingFrequency = 1000;
+    const pingFrequency = 60000;
     
     setInterval(() => {
       ping.sys.probe(pingIPAddress, (active) => {
